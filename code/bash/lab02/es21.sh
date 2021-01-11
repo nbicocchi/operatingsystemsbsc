61105a51cf64b38620b82ccb0c18c5eec062a9e1
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2021-01-11 14:16:27 +0100

    update

--- code/bash/lab02/es21.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-touch "$1"
-chmod "$2" "$1"
-
-exit 0
-
--- code/bash/lab02/es22.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo '#!/bin/bash' > "$1"
-echo "echo \"Hello World!\"" >> "$1"
-echo "exit 0" >> "$1"
-
-exit 0
-
