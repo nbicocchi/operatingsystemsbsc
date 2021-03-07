71c1c6b4dad624901bdb8c63dd37eb8422a90b98
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2021-03-07 12:31:20 -0800

    update

---
+++ ubuntu-post-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get install mc vim gnome-desktop build-essentials htop chromium sublime-text
