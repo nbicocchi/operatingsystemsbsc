e8dc1b3fbfc2778fc294b398cf6e452a0e6b34ac
Nicola <nicola.bicocchi@gmail.com>
2021-04-08 09:07:26 -0700

    update

--- bash/code/lab03/es39.sh
+++ bash/code/lab03/es39.sh
@@ -37,7 +37,11 @@ esac
 # Main body
 count_f1=$(find "$1" -type f -readable -name "$2" | wc -l 2>/dev/null)
 count_f2=$(find "$1" -type f -readable -name "$3" | wc -l 2>/dev/null)
-echo "$2": "$count_f1"
-echo "$3": "$count_f2"
+
+if [ "$count_f1" -gt 0 -a "$count_f2" -gt 0 ]; then
+  echo "Test passed for" "$1" 
+  echo "$2": "$count_f1"
+  echo "$3": "$count_f2"
+fi
 
 exit 0
