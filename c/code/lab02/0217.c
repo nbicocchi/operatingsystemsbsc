fd0b0fbc438d0c5e84a8aef2efd8dd6e2a761637
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2021-05-24 23:17:49 +0200

    update

--- c/code/lab02/0217.c
+++ c/code/lab02/0217.c
@@ -2,8 +2,7 @@
 
 /*
  * Merges two subarrays of arr[].
- * First subarray is arr[l..m]
- * Second subarray is arr[m+1..r]
+ * First subarray is arr[l..m], second subarray is arr[m+1..r]
  */
 void merge(int arr[], int l, int m, int r) {
     int i, j, k;
@@ -19,10 +18,10 @@ void merge(int arr[], int l, int m, int r) {
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    /* Merge the temp arrays back into arr[l..r] */
+    i = 0; /* Initial index of first subarray */
+    j = 0; /* Initial index of second subarray */
+    k = l; /* Initial index of merged subarray */
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -35,16 +34,14 @@ void merge(int arr[], int l, int m, int r) {
         k++;
     }
 
-    /* Copy the remaining elements of L[], if there
-    are any */
+    /* Copy the remaining elements of L[], if there are any */
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    /* Copy the remaining elements of R[], if there
-    are any */
+    /* Copy the remaining elements of R[], if there are any */
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -58,11 +55,10 @@ void merge(int arr[], int l, int m, int r) {
  */
 void merge_sort(int arr[], int l, int r) {
     if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
+        /* Same as (l+r)/2, but avoids overflow for large l and h */
         int m = l + (r - l) / 2;
 
-        // Sort first and second halves
+        /* Sort first and second halves */
         merge_sort(arr, l, m);
         merge_sort(arr, m + 1, r);
 
